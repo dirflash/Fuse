b0292254d614a17689cc8d511f50e467634d950e
Aaron Davis <aaron.e.davis@gmail.com>
2023-02-09 09:24:37 -0700

    test message

--- fuse-mgr.py
+++ fuse-mgr.py
@@ -1,8 +1,9 @@
 import os
+import requests
+import json
 
 print("Hello World!")
 
-webex_bearer = os.environ["webex_bearer"]
 webex_bearer = os.environ["webex_bearer"]
 attachment = os.environ["attachment"]
 room_id = os.environ["room_id"]
@@ -10,10 +11,19 @@ room_type = os.environ["room_type"]
 person_id = os.environ["person_id"]
 person_email = os.environ["person_email"]
 
-print(webex_bearer)
-print(webex_bearer)
-print(attachment)
-print(room_id)
-print(room_type)
-print(person_id)
-print(person_email)
+
+url = "https://webexapis.com/v1/messages/"
+
+headers = {
+    "Authorization": webex_bearer,
+    "Content-Type": "application/json",
+}
+
+payload = json.dumps(
+    {
+        "toPersonEmail": "aarodavi@cisco.com",
+        "markdown": "Test message.",
+    }
+)
+print(payload)
+r = requests.request("POST", url, headers=headers, data=payload, timeout=2)
