e3d6b192acacf95b9150ec5e8ed313f24cda91a6
Aaron Davis <aaron.e.davis@gmail.com>
2023-02-09 08:31:40 -0700

    secrets not working

--- fuse-mgr.py
+++ fuse-mgr.py
@@ -2,6 +2,7 @@ import os
 
 print("Hello World!")
 
+env_webex_bearer = os.environ["env_webex_bearer"]
 webex_bearer = os.environ["webex_bearer"]
 attachment = os.environ["attachment"]
 room_id = os.environ["room_id"]
@@ -9,6 +10,7 @@ room_type = os.environ["room_type"]
 person_id = os.environ["person_id"]
 person_email = os.environ["person_email"]
 
+print(env_webex_bearer)
 print(webex_bearer)
 print(attachment)
 print(room_id)
