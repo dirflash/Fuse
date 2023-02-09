65ea2464a5c4b46d379012a9e030d0e04f472f68
Aaron Davis <aaron.e.davis@gmail.com>
2023-02-08 18:58:49 -0700

    test

--- fuse-mgr.py
+++ fuse-mgr.py
@@ -1,1 +1,17 @@
+import os
+
 print("Hello World!")
+
+webex_bearer = os.environ["WEBEX_BEARER"]
+attachment = os.environ["attachment"]
+room_id = os.environ["room_id"]
+room_type = os.environ["room_type"]
+person_id = os.environ["person_id"]
+person_email = os.environ["person_email"]
+
+print(webex_bearer)
+print(attachment)
+print(room_id)
+print(room_type)
+print(person_id)
+print(person_email)
