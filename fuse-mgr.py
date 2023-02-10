8654e6c57418a7d5eebf28c79947e775bf6dc29c
Aaron Davis <aaron.e.davis@gmail.com>
2023-02-09 20:57:32 -0700

    Add msg attach logic

--- fuse-mgr.py
+++ fuse-mgr.py
@@ -1,9 +1,10 @@
 import os
+import sys
 import requests
 import json
-import pandas
-
-print("Hello World!")
+import csv
+import pandas as pd
+import configparser
 
 webex_bearer = os.environ["webex_bearer"]
 attachment = os.environ["attachment"]
@@ -12,36 +13,123 @@ room_type = os.environ["room_type"]
 person_id = os.environ["person_id"]
 person_email = os.environ["person_email"]
 
-
-post_msg = "https://webexapis.com/v1/messages/"
-
 headers = {
     "Authorization": webex_bearer,
     "Content-Type": "application/json",
 }
 
-message = "Attachment ID: " + attachment + " from " + person_id
+RAW_FILE_NAME = ""
 
-payload = json.dumps(
-    {
-        "toPersonEmail": "aarodavi@cisco.com",
-        "markdown": message,
-    }
-)
-print(payload)
-post_msg_r = requests.request(
-    "POST", post_msg, headers=headers, data=payload, timeout=2
-)
-post_msg_r.raise_for_status()
-print(f"Message sent ({post_msg_r.status_code})")
 
-get_attach_response = requests.request("GET", attachment, headers=headers, timeout=2)
-get_attach_response.raise_for_status()
-print(f"Message sent ({get_attach_response.status_code})")
+def alias_format(dframe):
+    """Pull the full name and alias from the "name" column and put them in separate columns.
 
-print(get_attach_response.text)
+    Args:
+        dframe (dataframe): Pandas dataframe
 
-attach_response = get_attach_response.json()
+    Returns:
+        dataframe: dataframe with new 'Full Name' and 'Alias' column
+    """
+    dframe[["Full Name", "Alias"]] = dframe["Name"].apply(
+        lambda x: pd.Series(str(x).split("("))
+    )
+    dframe["Alias"] = dframe["Alias"].str.replace(r"\)", "", regex=True)
+    # print(dframe.head())
+    print(f"{len(dframe)} entries.")
+    return dframe
 
-df = pandas.DataFrame(attach_response)
-print(df.head())
+
+def x_dups(dframe):
+    duplicateRows = dframe[dframe.duplicated(["Name"])]
+    print(f"Found {len(duplicateRows)} duplicate entries.")
+    dframe = dframe.drop_duplicates(subset="Name", keep="first")
+    dups = len(dframe[dframe.duplicated(["Name"])])
+    if dups == 0:
+        print("Duplicate rows removed.")
+    else:
+        print(f"Unable to remove {dups} duplicate rows.")
+    return dframe
+
+
+def responses(dframe):
+    no_respond = dframe[dframe["Response"] == "None"]
+    print(f"Not responded: {len(no_respond)}")
+    return no_respond
+
+
+def post_noncommited(nc, no_nc, email):
+    post_msg = "https://webexapis.com/v1/messages/"
+    pl_title = "**Number of noncommited attendees: " + str(no_nc) + "**" + "\n" + nc
+    payload = json.dumps(
+        {
+            "toPersonEmail": email,
+            "markdown": pl_title,
+        }
+    )
+    try:
+        post_msg_r = requests.request(
+            "POST", post_msg, headers=headers, data=payload, timeout=2
+        )
+        post_msg_r.raise_for_status()
+        print(f"Noncommited List Message sent ({post_msg_r.status_code})")
+    except requests.exceptions.Timeout:
+        print("Timeout error. Try again.")
+    except requests.exceptions.TooManyRedirects:
+        print("Bad URL")
+    except requests.exceptions.HTTPError as nc_err:
+        raise SystemExit(nc_err) from nc_err
+    except requests.exceptions.RequestException as nc_cat_exception:
+        raise SystemExit(nc_cat_exception) from nc_cat_exception
+
+
+try:
+    # get file attachment
+    get_attach_response = requests.request(
+        "GET", attachment, headers=headers, timeout=2
+    )
+    get_attach_response.raise_for_status()
+    print(f"attachment received: ({get_attach_response.status_code})")
+    RAW_FILE_NAME = get_attach_response.headers["content-disposition"]
+except requests.exceptions.Timeout:
+    print("Timeout error. Try again.")
+except requests.exceptions.TooManyRedirects:
+    print("Bad URL")
+except requests.exceptions.HTTPError as err:
+    raise SystemExit(err) from err
+except requests.exceptions.RequestException as cat_exception:
+    raise SystemExit(cat_exception) from cat_exception
+
+if RAW_FILE_NAME:
+    file = RAW_FILE_NAME.split('"')[1::2]
+    file_name = file[0]
+    print(f"Attachment file name: {file_name}")
+else:
+    print("Empty file attachment!")
+    raise SystemExit()
+
+try:
+    attach = get_attach_response.text
+    with open(file_name, "w", newline="\r\n", encoding="utf-8") as out:
+        out.write(attach)
+except:
+    print("Unable to convert attachment to file.")
+    raise SystemExit()
+
+try:
+    df = pd.read_csv(file_name, header=0)
+    # print(df.head())
+except OSError as e:
+    sys.exit("CSV file not found!")
+
+df1 = alias_format(df)
+df2 = x_dups(df1)
+no_resp = responses(df2)
+
+noncommited = df2[
+    (df2["Response"] == "None") & (df2["Attendance"] == "Required Attendee")
+]
+num_noncommited = len(noncommited)
+print(f"\nNoncommited Attendees: {num_noncommited}")
+noncommited_string = noncommited[["Full Name"]].to_string(index=False, header=False)
+
+post_noncommited(noncommited_string, num_noncommited, person_email)
