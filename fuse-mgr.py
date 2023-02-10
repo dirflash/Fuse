c86bf62fa62dbf0350df99a116912a41e12c4ea1
Aaron Davis <aaron.e.davis@gmail.com>
2023-02-09 21:38:47 -0700

    msg edit

--- fuse-mgr.py
+++ fuse-mgr.py
@@ -62,7 +62,7 @@ def post_noncommited(nc, no_nc, email):
     pl_title = (
         "## Number of noncommited attendees: "
         + str(no_nc)
-        + "\n"
+        + " \n"
         + nc
         + "\n\n### Sending reminders."
     )
