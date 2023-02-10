2bb7b3081d19d21a8cb12d8bca30caf59da7bb5e
Aaron Davis <aaron.e.davis@gmail.com>
2023-02-09 21:32:08 -0700

    removed a print

--- fuse-mgr.py
+++ fuse-mgr.py
@@ -174,4 +174,3 @@ noncommited_string = noncommited[["Full Name"]].to_string(index=False, header=Fa
 post_noncommited(noncommited_string, num_noncommited, person_email)
 
 NONCOMMITED_LST = noncommited["Alias"].values.tolist()
-print(NONCOMMITED_LST)
