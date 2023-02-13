2b89cb1876fdf5a8f6ad9967c993817f21b8222b
Aaron Davis <aaron.e.davis@gmail.com>
2023-02-13 09:48:53 -0700

    Added to attendance report

--- fuse-mgr-respond.py
+++ fuse-mgr-respond.py
@@ -1,5 +1,9 @@
 import os
 import sys
+import json
+import configparser
+import requests
+import pandas as pd
 
 KEY = "CI"
 if os.getenv(KEY):
@@ -7,16 +11,63 @@ if os.getenv(KEY):
     webex_bearer = os.environ["webex_bearer"]
     room_id = os.environ["room_id"]
     person_id = os.environ["person_id"]
+    person_email = os.environ["person_email"]
     action = os.environ["action"]
     auth_mgrs = os.environ["auth_mgrs"]
 else:
-    sys.exit()
+    print("Running locally.")
+    config = configparser.ConfigParser()
+    config.read("./secrets/config.ini")
+    webex_bearer = config["DEFAULT"]["webex_key"]
+    attachment = config["DEFAULT"]["attachment"]
+    room_id = config["DEFAULT"]["room_id"]
+    person_email = config["DEFAULT"]["person_email"]
+    auth_mgrs = config["DEFAULT"]["auth_mgrs"]
+    action = "attend_report"
 
-print(person_id)
+
+post_msg_url = "https://webexapis.com/v1/messages/"
+
+headers = {
+    "Authorization": webex_bearer,
+    "Content-Type": "application/json",
+}
+
+RAW_FILE_NAME = ""
+NONCOMMITED_LST = []
 
 
-def attend_report():
+def attend_report(silent_response, confirmed_response, denied_response, email):
     print("Attendance Report")
+    # print(silent_response.head())
+    pl_title = (
+        "Number of noncommited attendees: "
+        + str(len(silent_response))
+        + "\nNumber of confirmed attendees: "
+        + str(len(confirmed_response))
+        + "\nNumber of declined attendees: "
+        + str(len(denied_response))
+    )
+    payload = json.dumps(
+        {
+            "toPersonEmail": email,
+            "markdown": pl_title,
+        }
+    )
+    try:
+        post_msg_r = requests.request(
+            "POST", post_msg_url, headers=headers, data=payload, timeout=2
+        )
+        post_msg_r.raise_for_status()
+        print(f"Attendance Report Message sent ({post_msg_r.status_code})")
+    except requests.exceptions.Timeout:
+        print("Timeout error. Try again.")
+    except requests.exceptions.TooManyRedirects:
+        print("Bad URL")
+    except requests.exceptions.HTTPError as nc_err:
+        raise SystemExit(nc_err) from nc_err
+    except requests.exceptions.RequestException as nc_cat_exception:
+        raise SystemExit(nc_cat_exception) from nc_cat_exception
 
 
 def noncomit_reminders():
@@ -31,9 +82,70 @@ def survey_msg():
     print("Post Event Survey")
 
 
-if action == "attend_report":
-    attend_report()
-elif action == "noncomit_reminders":
+def not_authd_mgr(email):
+    post_msg = "https://webexapis.com/v1/messages/"
+    pl_title = "**You don't appear to be an authorized manager of this bot. Mamma told me not to talk to strangers.**"
+    payload = json.dumps(
+        {
+            "toPersonEmail": email,
+            "markdown": pl_title,
+        }
+    )
+    try:
+        post_msg_r = requests.request(
+            "POST", post_msg, headers=headers, data=payload, timeout=2
+        )
+        post_msg_r.raise_for_status()
+        print(f"Not Authorized Manager Message sent ({post_msg_r.status_code})")
+    except requests.exceptions.Timeout:
+        print("Timeout error. Try again.")
+    except requests.exceptions.TooManyRedirects:
+        print("Bad URL")
+    except requests.exceptions.HTTPError as nc_err:
+        raise SystemExit(nc_err) from nc_err
+    except requests.exceptions.RequestException as nc_cat_exception:
+        raise SystemExit(nc_cat_exception) from nc_cat_exception
+
+
+def alias_format(dframe):
+    """Pull the full name and alias from the "name" column and put them in separate columns.
+
+    Args:
+        dframe (dataframe): Pandas dataframe
+
+    Returns:
+        dataframe: dataframe with new 'Full Name' and 'Alias' column
+    """
+    dframe[["Full Name", "Alias"]] = dframe["Name"].apply(
+        lambda x: pd.Series(str(x).split("("))
+    )
+    dframe["Alias"] = dframe["Alias"].str.replace(r"\)", "", regex=True)
+    # print(dframe.head())
+    print(f"{len(dframe)} entries.")
+    return dframe
+
+
+def x_dups(dframe):
+    duplicateRows = dframe[dframe.duplicated(["Name"])]
+    print(f"Found {len(duplicateRows)} duplicate entries.")
+    dframe = dframe.drop_duplicates(subset="Name", keep="first")
+    dups = len(dframe[dframe.duplicated(["Name"])])
+    if dups == 0:
+        print("Duplicate rows removed.")
+    else:
+        print(f"Unable to remove {dups} duplicate rows.")
+    return dframe
+
+
+def responses(dframe):
+    no_respond = dframe[dframe["Response"] == "None"]
+    # print(f"Not responded: {len(no_respond)}")
+    y_respond = dframe[dframe["Response"] == "Accepted"]
+    d_respond = dframe[dframe["Response"] == "Declined"]
+    return (no_respond, y_respond, d_respond)
+
+
+if action == "noncomit_reminders":
     noncomit_reminders()
 elif action == "pre_reminder":
     pre_reminder()
@@ -42,7 +154,7 @@ elif action == "survey_msg":
 else:
     print("Unknown action.")
 
-if person_id in auth_mgrs:
+if person_email in auth_mgrs:
     print("Authorized manager.")
 else:
     print("Not an authorized manager.")
@@ -84,6 +196,13 @@ except:
 
 try:
     df = pd.read_csv(file_name, header=0)
-    # print(df.head())
+    print(df.head())
 except OSError as e:
     sys.exit("CSV file not found!")
+
+df1 = alias_format(df)
+df2 = x_dups(df1)
+no_resp, yes_respond, declined_respond = responses(df2)
+
+if action == "attend_report":
+    attend_report(no_resp, yes_respond, declined_respond, person_email)
