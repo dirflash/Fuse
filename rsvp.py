4755585fc1faeb14947f8dd2d90c2753529b5efc
Aaron Davis <aaron.e.davis@gmail.com>
2023-03-07 17:25:07 -0700

    housekeeping

--- rsvp.py
+++ rsvp.py
@@ -29,7 +29,7 @@ else:
     mongo_un = config["MONGO"]["MONGO_UN"]
     mongo_pw = config["MONGO"]["MONGO_PW"]
     rsvp_collect = config["MONGO"]["RSVP_COLLECT"]
-    rsvp_list = '["Aaron Hagen", "Darrell Lee", "Khoi Pham", "Minh Nguyen", "Derrick Martin", "Jaime Moreno", "Christopher Ronderos", "Robert Jackson", "Sean Huston", "Eric Kalisek", "Wade Vick", "Buddy Mckamey", "Dedra Cannon", "Joe Mejica", "Lester Marquez", "Tung Nguyen", "Nathan Larsen", "Ralph Herr", "Alfredo Jurado", "Keegan Uchacz", "Michael Lipsey", "Raffi Apardian", "Robert Boener", "Julie Palmer", "Majed Alwineyan", "Mike Beller", "Nick Fossen", "Aaron Davis", "Adam Gray", "Justin Damele", "Mike Nipp", "Rob Routt", "Kris Vassallo", "Matt Okuma", "Paul Gately", "Randall Crumm", "John Jackson", "Rama Subramanian", "Brian Lai", "Derron Cyrus"]'
+    rsvp_list = '["Brian Lai", "Derron Cyrus"]'
 
 
 MAX_MONGODB_DELAY = 500
@@ -243,137 +243,7 @@ for x in rsvp_l_list:
             thrott_back = send_rsvp_msg(rsvp_card, rsvp_email, x)
             print("Requested to throttle back twice. Something is wrong.")
             failed_msg_mgr()
-
-
-"""
-g = rsvp_collection.find().sort("_id", -1).limit(1)
-for _ in g:
-    if str(_["_id"]) == mongo_rec_id:
-        id_check = True
-        emails = _["survey_lst"]
-        for inx, person in enumerate(emails):
-            inx_plus = inx + 1
-            num_emails = len(emails)
-            msg_stat = send_survey_msgs(
-                inx_plus,
-                person,
-                first_name,
-                num_emails,
-                session_date,
-                survey_url,
-            )
-            if msg_stat == 429:
-                print("Retry send after 429 pause.")
-                msg_stat = send_survey_msgs(
-                    inx_plus,
-                    person,
-                    first_name,
-                    num_emails,
-                    session_date,
-                    survey_url,
-                )
-            print(f"Message sent to {person}: {msg_stat}")
+            sys.exit(1)
     else:
-        id_check = False
+        failed_msg_mgr()
         sys.exit(1)
-"""
-
-
-"""
-def noncomit_reminders(noes):
-    no_rsvp_email = []
-    print("Requested action: Send Non Committed Reminders")
-    noncommited = noes[
-        (noes["Response"] == "None") & (noes["Attendance"] == "Required Attendee")
-    ]
-    num_noncommited = len(noncommited)
-    print(f"\nNoncommited Attendees: {num_noncommited}")
-    # The following 3 lines takes the "Full Name" column, converts it to a list, then to
-    # a string to solve formatting issues in the "send_confirmation" function.
-    noncommited_names = noncommited[["Full Name"]]
-    noncommited_list = noncommited_names["Full Name"].to_list()
-    noncommited_list2str = "\n".join(str(e) for e in noncommited_list)
-    noncommited_alias_lst = noncommited[
-        "Alias"
-    ].values.tolist()  # list of email addresses to send reminder.
-    for _ in noncommited_alias_lst:
-        no_rsvp_email.append(_ + "@cisco.com")
-    send_confirmation(noncommited_list2str, no_rsvp_email, num_noncommited, person_id)
-    return no_rsvp_email
-
-
-def send_confirmation(nc, nc_emails, no_nc, email):
-    # Need to create confirmation card
-    nnc = str(no_nc)
-    post_msg = "https://webexapis.com/v1/messages/"
-
-    for nce in nc_emails:
-        pl_title = f"Hello, {nce}. You haven't confirmed."
-        payload = json.dumps(
-            {
-                "toPersonEmail": email,
-                "markdown": pl_title,
-            }
-        )
-        try:
-            post_msg_r = requests.request(
-                "POST", post_msg, headers=headers, data=payload, timeout=2
-            )
-            post_msg_r.raise_for_status()
-            print(f"Noncommited Message sent to {nce} - ({post_msg_r.status_code})")
-        except requests.exceptions.Timeout:
-            print(f"Timeout error. Try again. Line: {sys._getframe().f_lineno}")
-        except requests.exceptions.TooManyRedirects:
-            print("Bad URL")
-        except requests.exceptions.HTTPError as nc_err:
-            raise SystemExit(nc_err) from nc_err
-        except requests.exceptions.RequestException as nc_cat_exception:
-            raise SystemExit(nc_cat_exception) from nc_cat_exception
-
-
-def send_survey_gh(p_id, f_name, act, sess_date, surv_url, mong_id):
-    gh_headers = {
-        "Accept": "application/vnd.github.v3+json",
-        "Content-Type": "application/json",
-        "Authorization": github_pat,
-    }
-    gh_payload = json.dumps(
-        {
-            "event_type": "FUSE_SEND_SURVEYS",
-            "client_payload": {
-                "person_id": p_id,
-                "first_name": f_name,
-                "action": act,
-                "session_date": sess_date,
-                "survey_url": surv_url,
-                "mongo_id": mong_id,
-            },
-        }
-    )
-
-    gh_webhook_url = "https://api.github.com/repos/dirflash/fuse/dispatches"
-    try:
-        gh_webhook_response = requests.request(
-            "POST", gh_webhook_url, headers=gh_headers, data=gh_payload, timeout=2
-        )
-        gh_webhook_response.raise_for_status()
-        print(f"Confirmation Message sent ({gh_webhook_response.status_code})")
-    except requests.exceptions.Timeout:
-        print("GitHub Action punt timed out. Try again.")
-    except requests.exceptions.TooManyRedirects:
-        print("Bad URL")
-    except requests.exceptions.HTTPError as nc_err:
-        raise SystemExit(nc_err) from nc_err
-    except requests.exceptions.RequestException as nc_cat_exception:
-        raise SystemExit(nc_cat_exception) from nc_cat_exception
-
-
-def survey_to_mongo(surv_lst, pern_id):
-    ts = datetime.now()
-    record = survey_collection.insert_one(
-        {"ts": ts, "person_email": pern_id, "survey_lst": surv_lst}
-    )
-    record_id = record.inserted_id
-    print(f"Inserted Object ID: {record_id}")
-    return str(record_id)
-"""
